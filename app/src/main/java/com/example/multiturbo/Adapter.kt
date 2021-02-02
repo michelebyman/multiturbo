adcbfbbcfb18bac323c5ef0e5bd768257e27c3c0
Michele Byman <michelebyman@gmail.com>
2021-02-02 11:32:32 +0100

    fixed design button last row unecpected error

--- app/src/main/java/com/example/multiturbo/Adapter.kt
+++ app/src/main/java/com/example/multiturbo/Adapter.kt
@@ -1,22 +1,25 @@
 package com.example.multiturbo
 
+import android.content.res.ColorStateList
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
-
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 class Adapter() : RecyclerView.Adapter<ViewHolder>() {
 
-
+    lateinit var mainact : MainActivity
 
     var multy = mutableListOf<String>()
     var lastRowButtonColors = mutableListOf<String>("#F183AE", "#DC89A6", "#CA8DC1", "#AE90C6", "#98A4D0", "#88C1E7", "#87CEC5", "#ABCA91", "#DCD490", "#E79F8D")
-
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vh = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.button, parent, false))
@@ -40,92 +43,155 @@ class Adapter() : RecyclerView.Adapter<ViewHolder>() {
   
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        holder.button.isEnabled = true
+        holder.button.textSize = 14f
+        holder.button.setBackgroundColor(Color.parseColor("#FF0000"))
 
         val colnumber = (position/11)+1
         val rownumber = 10-(position%11)
 
-
         holder.button.text = ("$rownumber *  $colnumber").toString()
 
-        holder.button.setBackgroundColor(Color.CYAN)
+
 
         holder.button.setOnClickListener {
-            holder.button.text = (rownumber *  colnumber).toString()
 
+            //holder.button.text = (rownumber *  colnumber).toString()
+
+            Log.i("LETSDEBUG", "CLICK ITEM")
+            //Toast.makeText(mainact.applicationContext, "Test", Toast.LENGTH_LONG).show()
+
+            val builder = AlertDialog.Builder(mainact)
+            builder.setTitle(("$rownumber x  $colnumber").toString() + " = " + (rownumber *  colnumber).toString())
+            builder.setMessage("")
+
+
+            builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+
+            }
+
+            builder.show()
+            //builder.getWindow().setback
         }
 
 
+        var itemColor = lastRowButtonColors[9]
+
         if(rownumber <= 10 && colnumber <= 10)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[9]))
+            itemColor = lastRowButtonColors[9]
+            //holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[9]))
         }
 
         if(rownumber <= 9 && colnumber <= 9)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[8]))
+          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[8]))
+            itemColor = lastRowButtonColors[8]
         }
         if(rownumber <= 8 && colnumber <= 8)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[7]))
+          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[7]))
+            itemColor = lastRowButtonColors[7]
         }
         if(rownumber <= 7 && colnumber <= 7)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[6]))
+           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[6]))
+            itemColor = lastRowButtonColors[6]
         }
 
         if(rownumber <= 6 && colnumber <= 6)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[5]))
+           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[5]))
+
+            itemColor = lastRowButtonColors[5]
         }
 
         if(rownumber <= 5 && colnumber <= 5)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[4]))
+           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[4]))
+
+            itemColor = lastRowButtonColors[4]
         }
 
         if(rownumber <= 4 && colnumber <= 4)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[3]))
+         //   holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[3]))
+
+            itemColor = lastRowButtonColors[3]
         }
 
         if(rownumber <= 3 && colnumber <= 3)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[2]))
+          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[2]))
+
+            itemColor = lastRowButtonColors[2]
         }
         if(rownumber <= 2 && colnumber <= 2)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[1]))
+           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[1]))
+
+            itemColor = lastRowButtonColors[1]
         }
         if(rownumber == 1 && colnumber == 1)
         {
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[0]))
+          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[0]))
+            itemColor = lastRowButtonColors[0]
         }
 
-        if(rownumber == 0)
+        holder.button.setBackgroundColor(Color.parseColor(itemColor))
+
+         if(rownumber == 1)
         {
-            holder.button.text = (colnumber).toString()
+
             holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[colnumber-1]))
 
 
             if(colnumber == 1)
             {
-                holder.button.setBackgroundColor(Color.BLACK)
+                holder.button.setBackgroundResource(R.drawable.bottomleftradius)
+                holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.one)))
             }
             if(colnumber == 10)
             {
-                holder.button.setBackgroundColor(Color.MAGENTA)
+                holder.button.setBackgroundResource(R.drawable.bottomrightradius)
+                holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.ten)))
             }
 
         }
+
+
+
         if(rownumber == 10 && (colnumber == 1))
         {
-            holder.button.setBackgroundColor(Color.GRAY)
+            holder.button.setBackgroundResource(R.drawable.topleftradius)
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.ten)))
         }
 
         if(rownumber == 10 && (colnumber == 10))
         {
-            holder.button.setBackgroundColor(Color.RED)
+            holder.button.setBackgroundResource(R.drawable.toprightradius)
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.ten)))
         }
+
+        if (rownumber == 0) {
+            holder.button.text = (colnumber).toString()
+
+            holder.button.setBackgroundColor(Color.TRANSPARENT)
+            holder.button.isEnabled = false
+            holder.button.textSize = 30f
+          /* for (item in 1..10){
+                if (colnumber == item){
+                    holder.button.setTextColor(Color.parseColor(lastRowButtonColors[item]))
+                }
+            }
+
+           */
+
+
+
+        }
+
+
 
 
     }
--- app/src/main/java/com/example/multiturbo/MainActivity.kt
+++ app/src/main/java/com/example/multiturbo/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.multiturbo
 
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,9 +19,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ourAdapter.mainact = this
+
         var ourRecycleView = findViewById<RecyclerView>(R.id.buttonGrid)
+
+        //ourRecycleView.addItemDecoration(MarginItemDecoration(20))
 
         ourRecycleView.layoutManager = GridLayoutManager(this, 11, GridLayoutManager.HORIZONTAL,false)
         ourRecycleView.adapter = ourAdapter
+
+        var button63 = findViewById<Button>(R.id.button63)
+        button63.isEnabled = false
     }
 }
+
+/*
+class MarginItemDecoration(private val spaceHeight: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View,
+                                parent: RecyclerView, state: RecyclerView.State) {
+        with(outRect) {
+            if (parent.getChildAdapterPosition(view) == 10) {
+                top = spaceHeight
+            }
+            left =  0
+            right = 0
+            bottom = 0
+        }
+    }
+}
+
+ */
