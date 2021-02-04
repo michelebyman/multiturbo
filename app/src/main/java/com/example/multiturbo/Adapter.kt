5c8caf29493f3460e2dac300a151348f429122ea
Michele Byman <michelebyman@gmail.com>
2021-02-04 12:31:52 +0100

    recycler view fixed

--- app/src/main/java/com/example/multiturbo/Adapter.kt
+++ app/src/main/java/com/example/multiturbo/Adapter.kt
@@ -18,34 +18,28 @@ class Adapter() : RecyclerView.Adapter<ViewHolder>() {
 
     lateinit var mainact : MainActivity
 
-    var multy = mutableListOf<String>()
+
     var lastRowButtonColors = mutableListOf<String>("#F183AE", "#DC89A6", "#CA8DC1", "#AE90C6", "#98A4D0", "#88C1E7", "#87CEC5", "#ABCA91", "#DCD490", "#E79F8D")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vh = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.button, parent, false))
         return vh
-        createMultyList()
+
     }
 
     override fun getItemCount(): Int {
-        //return multy.size
+
         return 110
     }
 
-    fun createMultyList() {
-
-       /* for (i in 1..100) {
-            multy.add(i.toString())
-        }
-        */
-    }
 
   
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         holder.button.isEnabled = true
         holder.button.textSize = 14f
-        holder.button.setBackgroundColor(Color.parseColor("#FF0000"))
+
+        holder.button.setBackgroundResource(R.drawable.noradius)
 
         val colnumber = (position/11)+1
         val rownumber = 10-(position%11)
@@ -73,81 +67,76 @@ class Adapter() : RecyclerView.Adapter<ViewHolder>() {
             }
 
             builder.show()
-            //builder.getWindow().setback
+
         }
 
-
-        var itemColor = lastRowButtonColors[5]
 
 
 
         if(rownumber <= 10 && colnumber <= 10)
         {
-            itemColor = lastRowButtonColors[9]
-            //holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[9]))
+
+
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.ten)))
+
         }
 
         if(rownumber <= 9 && colnumber <= 9)
         {
-          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[8]))
-            itemColor = lastRowButtonColors[8]
+
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.nine)))
+
         }
         if(rownumber <= 8 && colnumber <= 8)
         {
-          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[7]))
-            itemColor = lastRowButtonColors[7]
+
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.eight)))
         }
         if(rownumber <= 7 && colnumber <= 7)
         {
-           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[6]))
-            itemColor = lastRowButtonColors[6]
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.seven)))
         }
 
         if(rownumber <= 6 && colnumber <= 6)
         {
-           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[5]))
-
-            itemColor = lastRowButtonColors[5]
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.six)))
         }
 
         if(rownumber <= 5 && colnumber <= 5)
         {
-           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[4]))
-
-            itemColor = lastRowButtonColors[4]
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.five)))
         }
 
         if(rownumber <= 4 && colnumber <= 4)
         {
-         //   holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[3]))
-
-            itemColor = lastRowButtonColors[3]
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.four)))
         }
 
         if(rownumber <= 3 && colnumber <= 3)
         {
-          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[2]))
-
-            itemColor = lastRowButtonColors[2]
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.three)))
         }
         if(rownumber <= 2 && colnumber <= 2)
         {
-           // holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[1]))
-
-            itemColor = lastRowButtonColors[1]
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.two)))
         }
         if(rownumber == 1 && colnumber == 1)
         {
-          //  holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[0]))
-            itemColor = lastRowButtonColors[0]
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.one)))
         }
 
-        holder.button.setBackgroundColor(Color.parseColor(itemColor))
 
-         if(rownumber == 1)
+
+
+
+
+
+
+
+        if(rownumber == 1)
         {
 
-            holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[colnumber-1]))
+            //holder.button.setBackgroundColor(Color.parseColor(lastRowButtonColors[colnumber-1]))
 
 
             if(colnumber == 1)
@@ -163,8 +152,6 @@ class Adapter() : RecyclerView.Adapter<ViewHolder>() {
 
         }
 
-
-
         if(rownumber == 10 && (colnumber == 1))
         {
             holder.button.setBackgroundResource(R.drawable.topleftradius)
@@ -177,10 +164,14 @@ class Adapter() : RecyclerView.Adapter<ViewHolder>() {
             holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.ten)))
         }
 
+
+
+
         if (rownumber == 0) {
             holder.button.text = (colnumber).toString()
 
-            holder.button.setBackgroundColor(Color.TRANSPARENT)
+            //holder.button.setBackgroundColor(Color.TRANSPARENT)
+            holder.button.setBackgroundTintList(ColorStateList.valueOf(holder.itemView.context.resources.getColor(R.color.transparent)))
             holder.button.isEnabled = false
             holder.button.textSize = 26f
           /* for (item in 1..10){
